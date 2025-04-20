bcfab0bf05fab1feed00a6497f014ba2d48e95a5
CryptonETH <149672650+cryptoneth@users.noreply.github.com>
2025-04-20 18:27:56 +0330

    Update gensyn_update.sh

--- gensyn_update.sh
+++ gensyn_update.sh
@@ -56,6 +56,6 @@ chmod +x ./run_rl_swarm.sh
 
 # Start a new screen session for Gensyn
 echo "Starting new screen session for Gensyn..."
-screen -dmS gensyn bash -c "nohup ./run_rl_swarm.sh > gensyn.log 2>&1 & ; exec bash"
+screen -dmS gensyn bash -c "source .venv/bin/activate && ./run_rl_swarm.sh ; exec bash"
 
 echo "Gensyn node setup and execution completed!"
